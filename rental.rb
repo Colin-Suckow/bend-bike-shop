0e474a8782388b21a2d5781854c1ea310949f5f8
Colin Suckow <csuckow99@gmail.com>
2020-02-17 11:42:59 -0800

    Add bike to rental

--- rental.rb
+++ rental.rb
@@ -2,10 +2,11 @@ require 'date'
 
 class Rental
 
-    attr_accessor :checkout_time, :duration_in_hours
+    attr_accessor :checkout_time, :duration_in_hours, :bike
 
-    def initalize(duration, checkout_time = DateTime.now())
-        @checkout_time =  checkout_time
+    def initalize(bike, duration, checkout_time = DateTime.now())
+        @checkout_time = checkout_time
+        @bike = bike
         @duration_in_hours = duration
     end
 
