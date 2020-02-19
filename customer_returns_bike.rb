5e5183f12f1f7b2be47ad6c2f5a3db7e60d9946a
Joseph Ventrella <ventrejo@oregonstate.edu>
2020-02-19 11:50:42 -0800

    Implement driver

--- customer_returns_bike.rb
+++ customer_returns_bike.rb
@@ -1,5 +1,9 @@
 require_relative 'bicycle_return_workflow'
-
-workflow = BicycleReturnWorkflow
+require_relative 'bicycle'
+require_relative 'rental'
+test_bike = Bicycle.new(:performance)
+rental_duration = 2 * 60 * 60
+test_rental = Rental.new(test_bike, 6, rental_duration, 50)
+workflow = BicycleReturnWorkflow.new(test_rental)
 
 workflow.run
