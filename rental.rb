fe6997f5d51950a80e8ee7a5245cef8c2c544370
Joseph Ventrella <ventrejo@oregonstate.edu>
2020-02-19 10:38:18 -0800

    Add late fee variable

--- rental.rb
+++ rental.rb
@@ -4,10 +4,11 @@ class Rental
 
     attr_accessor :checkout_time, :duration_in_hours, :bike
 
-    def initalize(bike, duration, checkout_time = DateTime.now())
+    def initalize(bike, duration, checkout_time = DateTime.now(), late_fee)
         @checkout_time = checkout_time
         @bike = bike
         @duration_in_hours = duration
+        @late_fee =late_fee
     end
 
 end
