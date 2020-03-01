82968f442cb8f0ff3079af4fd9fa6990c1e04634
unknown <amberj.kolar@gmail.com>
2020-03-01 12:00:57 -0800

    Takes user input to remove reservation by id References #7

--- cancel_rental_workflow.rb
+++ cancel_rental_workflow.rb
@@ -1,8 +1,14 @@
 def CancelRentalWorkflow
-	def initialize(customer)
-		@customer = customer
-	end
 
-	def run()
-	end
+  def initialize(customer)
+    @customer = customer
+  end
+
+  def run
+    # puts "Hi #{customer.first}, which rental would you like to cancel?\n"
+    # for rental in customer.rental_list
+    #   puts rental
+    # end
+  end
+
 end
