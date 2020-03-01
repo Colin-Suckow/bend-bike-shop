005717e5951a6945c0431da7bf8345d14b069fdf
Joseph Ventrella <ventrejo@oregonstate.edu>
2020-03-01 12:56:48 -0800

    Create test objects in employee_updates_price

--- employee_updates_price.rb
+++ employee_updates_price.rb
@@ -1,5 +1,11 @@
 require_relative 'employee_updates_price_workflow'
+require_relative 'bicycle'
+require_relative 'rental'
+require_relative 'rental_price'
 
-workflow = UpdatePrice.new
+rental_price = RentalPrice.new(10, 20)
+test_bike = Bicycle.new(:performance)
+test_rental = Rental.new(test_bike, 6, DateTime.now()-1, rental_price)
+workflow = UpdatePrice.new()
 
 workflow.run
