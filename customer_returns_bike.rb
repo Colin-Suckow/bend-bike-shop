b54218f4dab956abb180eb979389f382271742e2
Joseph Ventrella <ventrejo@oregonstate.edu>
2020-03-01 15:17:38 -0800

    Add latefee to UpdatePrice

--- customer_returns_bike.rb
+++ customer_returns_bike.rb
@@ -2,6 +2,7 @@ require_relative 'bicycle_return_workflow'
 require_relative 'bicycle'
 require_relative 'rental'
 require_relative 'rental_price'
+
 test_bike = Bicycle.new(:performance)
 rental_price = RentalPrice.new(10, 20)
 test_rental = Rental.new(test_bike, 6, DateTime.now() - 1, rental_price) # Rented yesterday, returned today, 24 hours later
--- employee_updates_price_workflow.rb
+++ employee_updates_price_workflow.rb
@@ -1,10 +1,19 @@
 class UpdatePrice
-    def initialize(rentalPrice, rental)
+
+    def initialize(rentalPrice, newHourlyPrice, newLateFee)
     	@rentalPrice = rentalPrice
-    	@rental = rental
+    	@newHourlyPrice = newHourlyPrice
+    	@newLateFee = newLateFee
     end
 
     def run
-    
+    	current_hourly_price = @rentalPrice.hourlyPrice
+    	current_late_fee = @rentalPrice.lateFee
+    	puts "Original rental price: #{current_hourly_price}$ per hour.
+    		 	\rOriginal late fee: #{current_late_fee}$."
+    	current_hourly_price = @rentalPrice.hourlyPrice = @newHourlyPrice
+    	current_late_fee = @rentalPrice.lateFee = @newLateFee
+    	puts "New rental price: #{current_hourly_price}$ per hour.  
+   				\rNew late fee: #{current_late_fee}$."	
     end
 end
