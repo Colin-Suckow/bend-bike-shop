4fc40657fc7da4ca73968b315f4da4e206842ee9
Natashia White <whitenat@oregonstate.edu>
2020-03-01 12:02:12 -0800

    move methods to workflow references #7

--- cancel_rental_workflow.rb
+++ cancel_rental_workflow.rb
@@ -1,8 +1,20 @@
+
 def CancelRentalWorkflow
+
+	attr_accessor :customer
+
 	def initialize(customer)
 		@customer = customer
 	end
 
-	def run()
+	def run
+
+		puts "Hi #{test_customer.first}, which rental would you like to cancel?\n"
+
+		for rental in test_customer.rental_list
+			puts rental
+		end
+
 	end
+
 end
