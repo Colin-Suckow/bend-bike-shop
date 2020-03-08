7ca3b53fc252c0c46afe2519efc1d5f5f9c3c792
Joseph Ventrella <ventrejo@oregonstate.edu>
2020-03-08 12:54:03 -0700

    Implement change price methods in Update price workflow

--- employee_updates_price_workflow.rb
+++ employee_updates_price_workflow.rb
@@ -10,8 +10,8 @@ class UpdatePrice
     	puts "Original rental price: #{@rental_price.hourly_price}$ per hour.
     			\rOriginal late fee: #{@rental_price.late_fee}$."
     			
-    	@rental_price.hourly_price = @new_hourly_price
-    	@rental_price.late_fee = @new_late_fee
+    	@rental_price.change_hourly_price(@new_hourly_price)
+    	@rental_price.change_late_fee(@new_late_fee)
 
     	puts "New rental price: #{@rental_price.hourly_price}$ per hour.  
    				\rNew late fee: #{@rental_price.late_fee}$."
