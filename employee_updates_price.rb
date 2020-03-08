7ce0d9867f80a486ac5ff4f37d51a68f6d24ca47
Joseph Ventrella <ventrejo@oregonstate.edu>
2020-03-08 14:28:32 -0700

    Add kayak to employee_updates_price

--- employee_updates_price.rb
+++ employee_updates_price.rb
@@ -3,10 +3,16 @@ require_relative 'employee_updates_price_workflow'
 require_relative 'bicycle'
 require_relative 'rental'
 require_relative 'rental_price'
+require_relative 'kayak'
 
-rental_price = RentalPrice.new(10, 20)
+bike_price = RentalPrice.new(10, 20)
+kayak_price = RentalPrice.new(40, 60)
 test_bike = Bicycle.new(:performance)
-test_rental = Rental.new(2, test_bike, 6, DateTime.now()-1, rental_price)
-workflow = UpdatePrice.new(rental_price, 35, 100)
+test_kayak = Kayak.new(:whitewater)
+test_rental = Rental.new(2, test_bike, 6, DateTime.now()-1, bike_price)
+test_kayak_rental = Rental.new(3, test_kayak, 8, DateTime.now()-1, kayak_price)
+bike_workflow = UpdatePrice.new(bike_price, 35, 100)
+kayak_worflow = UpdatePrice.new(kayak_price, 45,65)
 
-workflow.run
+bike_workflow.run
+kayak_worflow.run
