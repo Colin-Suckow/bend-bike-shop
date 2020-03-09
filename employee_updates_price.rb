13169b29d2bfd3ba870af7a497ec68d6bc4b4c3a
Joseph Ventrella <ventrejo@oregonstate.edu>
2020-03-08 18:01:07 -0700

    Adjust workflow setup in employee_updates_price

--- employee_updates_price.rb
+++ employee_updates_price.rb
@@ -1,18 +1,18 @@
 require_relative 'rentable'
 require_relative 'employee_updates_price_workflow'
 require_relative 'bicycle'
-require_relative 'rental'
 require_relative 'rental_price'
 require_relative 'kayak'
+require_relative 'rental'
 
 bike_price = RentalPrice.new(10, 20)
-kayak_price = RentalPrice.new(40, 60)
-test_bike = Bicycle.new(:performance)
+kayak_price = RentalPrice.new(35, 45)
+test_bike = Bicycle.new(:mountain)
 test_kayak = Kayak.new(:whitewater)
-test_rental = Rental.new(2, test_bike, 6, DateTime.now()-1, bike_price)
-test_kayak_rental = Rental.new(3, test_kayak, 8, DateTime.now()-1, kayak_price)
-bike_workflow = UpdatePrice.new(bike_price, 35, 100)
-kayak_worflow = UpdatePrice.new(kayak_price, 45,65)
+kayak_rental = Rental.new(3, test_kayak, 8, DateTime.now()-1, kayak_price )
+bike_rental = Rental.new(2,test_bike, 6, DateTime.now() - 1, bike_price)
+bike_workflow = UpdatePrice.new(bike_rental.rental_price, 15, 30)
+kayak_worflow = UpdatePrice.new(kayak_rental.rental_price, 45,65)
 
 bike_workflow.run
 kayak_worflow.run
