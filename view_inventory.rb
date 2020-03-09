5733f10fa7a30549c56d80a70c26718b4b328ba1
Natashia White <whitenat@oregonstate.edu>
2020-03-08 19:43:11 -0700

    add kayaks to view inventory workflow references #18

--- view_inventory.rb
+++ view_inventory.rb
@@ -1,6 +1,7 @@
 # filename: view_inventory.rb
 require_relative 'rentable.rb'
 require_relative 'bicycle.rb'
+require_relative 'kayak'
 require_relative 'inventory_list.rb'
 require_relative 'view_inventory_workflow.rb'
 
@@ -8,18 +9,39 @@ inventory_list = InventoryList.new [
   Bicycle.new(:mountain),
   Bicycle.new(:mountain),
   Bicycle.new(:bmx),
-  Bicycle.new(:road)
+  Bicycle.new(:road),
+	Kayak.new(:whitewater),
+	Kayak.new(:recreational),
+	Kayak.new(:sport),
+	Kayak.new(:sport),
+
 ]
 
+puts"Do you want to rent a bike or a kayak?"
+rentaltype = gets.chomp!
 
-puts"what kind of bikes do you want?"
+if rentaltype == "bike"
 
-type = gets.chomp!.to_sym
+	puts"what kind of bikes do you want?"
 
-if Bicycle::TYPES.include?(type)
-  workflow = ViewInventoryWorkflow.new(inventory_list, [type])
-  workflow.run
+	biketype = gets.chomp!.to_sym
+
+	if Bicycle::TYPES.include?(biketype)
+	  workflow = ViewInventoryWorkflow.new(inventory_list, [biketype])
+	  workflow.run
+	else
+	  puts "Type not available"
+	end
+elsif rentaltype == "kayak"
+	puts "what kind of kayak do you want?"
+
+	kayaktype = gets.chomp!.to_sym
+	if Kayak::TYPES.include?(kayaktype)
+	  workflow = ViewInventoryWorkflow.new(inventory_list, [kayaktype])
+	  workflow.run
+	else
+		puts "Type not available"
+	end
 else
-  puts "Type not available"
+	puts "we only rent bikes or kayaks"
 end
-
