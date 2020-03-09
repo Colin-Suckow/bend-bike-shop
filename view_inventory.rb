5a2d444a8c016d0fa7ca547f24f77c35741bc1e8
Natashia White <whitenat@oregonstate.edu>
2020-03-08 19:10:05 -0700

    change naming conventions from bikes to rantables

--- view_inventory.rb
+++ view_inventory.rb
@@ -2,9 +2,10 @@
 
 require_relative 'view_inventory_workflow.rb'
 require_relative 'bicycle.rb'
+require_relative 'rentable.rb'
 require_relative 'inventory_list.rb'
 
-inventory_list = BikeList.new [ 
+inventory_list = InventoryList.new [ 
   Bicycle.new(:mountain),
   Bicycle.new(:mountain),
   Bicycle.new(:bmx),
